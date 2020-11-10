305a26fa068e9bd7b7e3b891cd296593500edab9
danteGiuliano <dante.giuliano@est.fi.uncoma.edu.ar>
2020-11-10 10:26:08 -0300

    change comment

--- actividadesGrupales/ProblemaSaludo/TestSaludo.java
+++ actividadesGrupales/ProblemaSaludo/TestSaludo.java
@@ -13,7 +13,7 @@ public class TestSaludo {
         }
         empleados.add(new Thread(new Jefe(unSaludo, "El PEPE")));// Empieza en el empleado 0
 
-        // Un bloque en Java usando expresiones lamdas.
+        // Un bloque en Java usando expresiones lambdas-introduccion a programacion funcional.
         empleados.forEach(e -> e.start());
         empleados.forEach(e -> {
             try {
